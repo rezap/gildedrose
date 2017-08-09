6f5d8325a91b6d94a0e71ba6c7ae04e2eaba8c45
rezap <rezap@users.noreply.github.com>
2017-08-09 16:42:00 +0200

    new commit

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-    @Test
+    #@Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
